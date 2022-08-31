edcb8f3a7d8936b0688ef93bc08ddb3d9b3f9f6b
Jeffrey Deng <jeffreytheflash123@gmail.com>
2022-08-31 18:20:29 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
            Want to get in touch? Feel free to shoot me an {''}
            <a
             className="underline decoration-neutral-300 dark:decoration-neutral-700"
-            href="mailto:cezhong2@illinois.edu"
+            href="mailto:jeffreydcz@gatech.edu"
             rel="noreferrer"
             target="_blank"
           >
