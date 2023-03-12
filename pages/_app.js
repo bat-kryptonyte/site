564f125ed51b517954c5ea1b90aac49d08bfe84f
cdeng65 <cdeng65@gatech.edu>
2023-03-12 00:19:15 -0500

    font change

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import '@/styles/global.css'
 import { ThemeProvider } from 'next-themes'
-import { MDXProvider } from '@mdx-js/react'
-import MDXComponents from '@/components/MdxComponents'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <MDXProvider components={MDXComponents}>
         <Component {...pageProps} />
-      </MDXProvider>
     </ThemeProvider>
   )
 }
